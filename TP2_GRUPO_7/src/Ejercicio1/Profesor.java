d95f87231b3ed89666e84cdfb96d4f23d1e53ad0
IsaiasS1985 <111474163+IsaiasS1985@users.noreply.github.com>
2023-04-07 19:08:28 -0300

    Update Profesor.java

--- TP2_GRUPO_7/src/Ejercicio1/Profesor.java
+++ TP2_GRUPO_7/src/Ejercicio1/Profesor.java
@@ -3,4 +3,23 @@ package Ejercicio1;
 public class Profesor extends Empleado {
 	private String cargo;
 	private int antiguedadDocente;
+	
+	public Profesor()
+	{
+		super();
+		this.cargo = "sin cargo";
+		this.antiguedadDocente = 0;
+	}
+	
+	public Profesor(String Nombre, int edad, String cargo, int antiguedadDocente) 
+	{
+		super(Nombre,edad);
+		this.cargo = cargo;
+		this.antiguedadDocente = antiguedadDocente;
+	}
+	@Override
+	public String toString() {
+		return "ID: " + getId() +", Nombre: " + getNombre() +", Edad: " + getEdad() +
+				", Cargo: " + cargo + ", Antiguedad docente: " + antiguedadDocente;
+	}
 }
