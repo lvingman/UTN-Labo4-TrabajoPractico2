5e699b9d86121a57aca142b712dfe09780980808
IsaiasS1985 <111474163+IsaiasS1985@users.noreply.github.com>
2023-04-12 18:23:07 -0300

    creo la clase padre Edificio

---
+++ TP2_GRUPO_7/src/Ejercicio3/Edificio.java
@@ -0,0 +1,31 @@
+package Ejercicio3;
+
+public class Edificio {
+
+	private double SuperficieEdificio;
+	
+	
+	public Edificio()
+	{
+		this.SuperficieEdificio = 0;
+	}
+	
+	public Edificio(double SuperficieEdificio)
+	{
+		this.SuperficieEdificio = SuperficieEdificio;
+	}
+	
+	public double getSuperficieEdificio() {
+		return SuperficieEdificio;
+	}
+
+	public void setSuperficieEdificio(double superficieEdificio) {
+		SuperficieEdificio = superficieEdificio;
+	}
+	
+	@Override
+	public String toString() {
+		return "Edificio [SuperficieEdificio: " + SuperficieEdificio + "]";
+	}
+	
+}
